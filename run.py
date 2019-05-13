50d058a4611bd34590c97a73667cdd25e6415cf9
Leihj <huijuan_lei@163.com>
2019-05-13 11:32:47 +0800

    update run.py文件

--- run.py
+++ run.py
@@ -12,9 +12,20 @@ import unittest
 
 from common import contants
 
-dis=unittest.defaultTestLoader.discover(contants.testcase_dir,"test_*.py")
+from testcase import test_register
+from testcase import test_recharge
+from testcase import test_login
+
+suite=unittest.TestSuite()
+load=loader=unittest.TestLoader()
+
+suite.addTests(load.loadTestsFromModule(test_register))
+suite.addTests(load.loadTestsFromModule(test_login))
+suite.addTests(load.loadTestsFromModule(test_recharge))
+
+# dis=unittest.defaultTestLoader.discover(contants.testcase_dir,"test_*.py")
 
 with open(contants.report_html+"/leihuijuan-test-report{}.html".format(time.strftime('%Y%m%d%H%M%S')),"wb+")as file:
     runner = HTMLTestRunnerNew.HTMLTestRunner \
         (stream=file, title="autointerface测试报告", description="2019年4月19日", tester="leihj")
-    runner.run(dis)
+    runner.run(suite)
